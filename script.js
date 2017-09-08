d5a3a2ab565b286b6bf64c0a3420de778aac74bf
Andrew Clavin <andrewclavin8@gmail.com>
2017-09-07 23:22:26 -0400

    little style bandaids

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log('running');
+var AC = {};
+/*var AC.dataModel = {
+    interests: {
+        initial: [
+            painting: {
+                text: 'painting',
+                image: 'test.png'
+            },
+            biking: {
+                text: 'exploring Cleveland by bike',
+                image: 'flowers.jpg'
+            }
+        ]
+    }
+};*/
