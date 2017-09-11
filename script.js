af3b619311644f4a0074591d64f8f8c5f0396690
Andrew Clavin <andrewclavin8@gmail.com>
2017-09-11 01:28:00 -0400

    functionality and some pictures

--- script.js
+++ script.js
@@ -3,38 +3,103 @@ AC.paint = {
     interests: [
         {
             name: 'painting',
-            text: 'paint',
-            image: 'test.png'
+            text: 'painting',
+            image: 'paintbrush.jpg'
         },
         {
             name: 'biking',
-            text: 'explore Cleveland by bike',
-            image: 'flowers.jpg'
+            text: 'exploring Cleveland by bike',
+            image: 'bike.jpg'
+        },
+        {
+            name: 'skyline',
+            text: 'staring out the window at downtown Cleveland',
+            image: 'skyline.jpg'
+        },
+        {
+            name: 'coffee',
+            text: 'making coffee',
+            image: 'face.png'
         },
         {
             name: 'kyoto',
-            text: 'design a makeshift kyoto coffee brewer',
-            image: 'test.png'
+            text: 'designing a makeshift Kyoto coffee brewer',
+            image: 'flowers.jpg'
         },
         {
             name: 'succulents',
-            text: 'proprogate my succulents',
+            text: 'trying to propogate succulents',
+            image: 'succulents.jpg'
+        },
+        {
+            name: 'french',
+            text: 'trying to learn french',
+            image: 'test.png'
+        },
+        {
+            name: 'cavs',
+            text: 'watching the cavs',
+            image: 'paisley.png'
+        },
+        {
+            name: 'kyrie',
+            text: 'missing Kyrie',
+            image: 'test.png'
+        },
+        {
+            name: 'sushi',
+            text: 'eating sushi',
+            image: 'test.png'
+        },
+        {
+            name: 'running',
+            text: 'running',
+            image: 'test.png'
+        },
+        {
+            name: 'piano',
+            text: 'playing piano',
+            image: 'test.png'
+        },
+        {
+            name: 'pickles',
+            text: 'pickling vegetables',
+            image: 'test.png'
+        },
+        {
+            name: 'meditating',
+            text: 'meditating',
+            image: 'test.png'
+        },
+        {
+            name: 'vox',
+            text: 'binging Vox and Ted-Ed videos',
             image: 'test.png'
         },
         {
             name: 'artMuseum',
-            text: 'stare at paintings at the cleveland Museum of art',
-            image: 'test.png'
+            text: 'staring at paintings at the cleveland Museum of art',
+            image: 'test2.png'
         },
         {
             name: 'read',
-            text: 'read the new palahniuk novel',
+            text: 'reading the new palahniuk novel',
             image: 'test.png'
         },
         {
             name: 'netflix',
-            text: 'binge the latest netflix original',
-            image: 'test.png'
+            text: 'binging the latest netflix original',
+            image: 'test2.png'
+        },
+        {
+            name: 'weezer',
+            text: 'listening to Weezer',
+            image: 'test2.png'
+        },
+        {
+            name: 'pierogies',
+            text: 'Eating Pierogies',
+            image: 'test2.png'
         }
     ]
 };
@@ -49,9 +114,32 @@ AC.canvas = {
     }
 };
 
-console.log(AC.canvas);
 AC.brush = function(canvas, paint){
+    let brush = {};
     let interestIndex;
+    function changeBackgroundImage(element, image){
+        let str = window.getComputedStyle(element, null).background;
+        let strStart = str.indexOf('url');
+        let strEnd =str.indexOf(')', strStart);
+        str = str.slice(0, strStart + 5) + image + str.slice(strEnd - 1);
+        element.style.background = str;
+        if(element.className.indexOf('js_frame_before') !== -1){
+            let str2 = window.getComputedStyle(element, 'before').background;
+            let strStart2 = str2.indexOf('url');
+            let strEnd2 =str2.indexOf(')', strStart2);
+            str2 = str2.slice(0, strStart2 + 5) + image + str2.slice(strEnd2 - 1);
+            document.styleSheets[0].addRule('.' + element.className.slice(0, element.className.indexOf(' ')) + ':before', 'background: ' + str2);
+            document.styleSheets[0].insertRule('.' + element.className.slice(0, element.className.indexOf(' ')) + ':before { background: ' + str2 + ' }', 0);
+        }
+        if(element.className.indexOf('js_frame_after') !== -1){
+            let str3 = window.getComputedStyle(element, 'after').background;
+            let strStart3 = str3.indexOf('url');
+            let strEnd3 =str3.indexOf(')', strStart3);
+            str3 = str3.slice(0, strStart3 + 5) + image + str3.slice(strEnd3 - 1);
+            document.styleSheets[0].addRule('.' + element.className.slice(0, element.className.indexOf(' ')) + ':after', 'background: ' + str3);
+            document.styleSheets[0].insertRule('.' + element.className.slice(0, element.className.indexOf(' ')) + ':after { background: ' + str3 + ' }', 0);
+        }
+    }
     Array.prototype.forEach.call(canvas.form.interestText, function(stroke, index, array){
         stroke.textContent = paint.interests[index].text;
         interestIndex = index;
@@ -59,12 +147,30 @@ AC.brush = function(canvas, paint){
     Array.prototype.forEach.call(canvas.interaction.changeInterests, function(thought){
         thought.onclick = function(event){
             let group = this.getAttribute('data-attr-interest-group');
+            const randomKey = Math.floor(Math.random() * Object.keys(canvas.form.primedFrames).length + 1).toString();
             for(var i = 0; i < canvas.form.interestText.length; i++){
                 if (group === canvas.form.interestText[i].getAttribute('data-attr-interest-group')){
-                    interestIndex = interestIndex + 1;
+                    interestIndex = Math.floor(Math.random() * paint.interests.length);
                     canvas.form.interestText[i].textContent = paint.interests[interestIndex].text;
                 }
             }
+            canvas.form.primedFrames[randomKey].forEach(function(element){
+                changeBackgroundImage(element, paint.interests[interestIndex].image);
+            });
         }
     });
+    brush.primeCanvas = function(){
+        console.log("yo", this, canvas, paint);
+        canvas.form.primedFrames = {};
+        Array.prototype.forEach.call(canvas.form.displayFrames, function(frame){
+            if(canvas.form.primedFrames[frame.getAttribute('data-attr-frame-group')]){
+                canvas.form.primedFrames[frame.getAttribute('data-attr-frame-group')].push(frame);
+            } else{
+                canvas.form.primedFrames[frame.getAttribute('data-attr-frame-group')] = [frame];
+            }
+        });
+        console.log("yo yo", canvas, canvas.form.primedFrames);
+    }
+    return brush;
 }(AC.canvas, AC.paint);
+AC.brush.primeCanvas();
